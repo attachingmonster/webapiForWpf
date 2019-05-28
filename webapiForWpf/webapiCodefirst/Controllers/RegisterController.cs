3627091b69325ffa4ca402de9dd1d989cf594763
zhg12581 <2634562735@qq.com>
2019-05-28 22:36:46 +0800

    注册账号和找回密码传输到webapi的加密问题

--- webapiForWpf/webapiCodefirst/Controllers/RegisterController.cs
+++ webapiForWpf/webapiCodefirst/Controllers/RegisterController.cs
@@ -31,48 +31,33 @@ namespace webapiCodefirst.Controllers
                if (viewmodelRegister.Account!= "")    //判断用户账号是否为空
                {
                    var u = unitOfWork.SysUserRepository.Get().Where(s => s.UserAccount.Equals(viewmodelRegister.Account)).FirstOrDefault();   //查找是否存在账号，存在返回账号所在对象，否则返回null
-                   if (u == null)    //判断账号是否存在
-                   {                     
-                       if (viewmodelRegister.Password != "")    //判断密码是否为空
-                       {                          
-                            if (viewmodelRegister.QuestionOrAnswer!= "你最喜欢的颜色是" || viewmodelRegister.QuestionOrAnswer != "你的生日是"|| viewmodelRegister.QuestionOrAnswer!= "你的父亲叫什么名字"|| viewmodelRegister.QuestionOrAnswer != "你最喜欢做什么"|| viewmodelRegister.QuestionOrAnswer != "你的梦想是")    //判断拾回密码是否为空
-                            {
-                                // 下面需要调试 看看 ，逻辑过程是否合理，另外角色根据具体场景，一般是默认缺省的角色，其他的角色在用户管理界面进行重新 赋予高级角色
-                                var sysRole = unitOfWork.SysRoleRepository.Get().Where(s => s.RoleName.Equals(viewmodelRegister.RoleName)).FirstOrDefault();    //寻找用户所选择角色在UserRole里的实例，返回对象
-                                if (sysRole != null)
-                                {
-                                    var CurrentUser = new SysUser();
-                                    CurrentUser.UserAccount = viewmodelRegister.Account;
-                                    CurrentUser.UserPassword = CreateMD5.EncryptWithMD5(viewmodelRegister.Password);
-                                    CurrentUser.UserAnswer = CreateMD5.EncryptWithMD5(viewmodelRegister.QuestionOrAnswer);
-                                    CurrentUser.RememberPassword = "0";
-                                    unitOfWork.SysUserRepository.Insert(CurrentUser);    //增加新SysUser
-                                    unitOfWork.Save();
+                    if (u == null)    //判断账号是否存在
+                    {
+                        // 下面需要调试 看看 ，逻辑过程是否合理，另外角色根据具体场景，一般是默认缺省的角色，其他的角色在用户管理界面进行重新 赋予高级角色
+                        var sysRole = unitOfWork.SysRoleRepository.Get().Where(s => s.RoleName.Equals(viewmodelRegister.RoleName)).FirstOrDefault();    //寻找用户所选择角色在UserRole里的实例，返回对象
+                        if (sysRole != null)
+                        {
+                            var CurrentUser = new SysUser();
+                            CurrentUser.UserAccount = viewmodelRegister.Account;
+                            CurrentUser.UserPassword = viewmodelRegister.Password;
+                            CurrentUser.UserAnswer = viewmodelRegister.QuestionOrAnswer;
+                            CurrentUser.RememberPassword = "0";
+                            unitOfWork.SysUserRepository.Insert(CurrentUser);    //增加新SysUser
+                            unitOfWork.Save();
 
-                                    var CurrentUserRole = new SysUserRole();
-                                    CurrentUserRole.SysUserID = CurrentUser.ID;
-                                    CurrentUserRole.SysRoleID = sysRole.ID;
-                                    unitOfWork.SysUserRoleRepository.Insert(CurrentUserRole);    //增加新SysUserRole
-                                    unitOfWork.Save();    //对更改进行保存
-                                    vMregisterInfomation.Message = "注册成功";
-                                    return vMregisterInfomation;
-
-                                }
-                            }
-                            else
-                            {
-                                throw new Exception("密码拾回问题答案不能为空！");
-                            }                                                   
-                       }
-                       else
-                       {
-                           throw new Exception("密码不能为空！");
-                       }
-                   }
-                   else
-                   {
-                       throw new Exception("用户名已存在！");
-                   }
+                            var CurrentUserRole = new SysUserRole();
+                            CurrentUserRole.SysUserID = CurrentUser.ID;
+                            CurrentUserRole.SysRoleID = sysRole.ID;
+                            unitOfWork.SysUserRoleRepository.Insert(CurrentUserRole);    //增加新SysUserRole
+                            unitOfWork.Save();    //对更改进行保存
+                            vMregisterInfomation.Message = "注册成功";
+                            return vMregisterInfomation;
+                        }
+                    }
+                    else
+                    {
+                        throw new Exception("用户名已存在！");
+                    }
                }
                else
                {
--- webapiForWpf/webapiCodefirst/Controllers/RetrievePswController.cs
+++ webapiForWpf/webapiCodefirst/Controllers/RetrievePswController.cs
@@ -28,78 +28,32 @@ namespace webapiCodefirst.Controllers
                 var u = unitOfWork.SysUserRepository.Get().Where(s => s.UserAccount.Equals(viewModelRetrievePsw.Account)).FirstOrDefault();  //查找是否存在账号
                 if (u != null)
                 {
-                    //把问题与答案一起加密 放在  QuestionOrAnswer 字段里
-                    if (viewModelRetrievePsw.QuestionOrAnswer != "你最喜欢的颜色是" || viewModelRetrievePsw.QuestionOrAnswer != "你的生日是" || viewModelRetrievePsw.QuestionOrAnswer != "你的父亲叫什么名字" || viewModelRetrievePsw.QuestionOrAnswer != "你最喜欢做什么" || viewModelRetrievePsw.QuestionOrAnswer != "你的梦想是")
-                    {
+                    //把问题与答案一起加密 放在  QuestionOrAnswer 字段里               
                         //这些信息都比较敏感，所以加密后再 发到 webapi  进行匹配
-                        if (u.UserAnswer.Equals(CreateMD5.EncryptWithMD5(viewModelRetrievePsw.QuestionOrAnswer)))
-                        {
-                            if (viewModelRetrievePsw.NewPassword != "")
-                            {
-                                //密码怎么样，由UI去判断，减少服务端的 载荷压力，包括下面的 密码一致，2次密码是否一致，都在 UI 里决解，暂时只有账号判断在 服务端 进行
-                                int number = 0, character = 0;
-                                foreach (char c in viewModelRetrievePsw.NewPassword)   //规范密码必须由ASCII码33~126之间的字符构成
-                                {
-                                    if (!(33 <= c && c <= 126))
-                                    {
-                                        throw new Exception("符号错误，请重新输入！");
-                                    }
-                                    if ('0' <= c && c <= '9') //number记录数字个数
-                                    {
-                                        number++;
-                                    }
-                                    else                      //character记录字符个数
-                                    {
-                                        character++;
-                                    }
-                                }
-                                if (number < 5 || character < 2)  //密码的安全系数
-                                {
-                                    throw new Exception("新密码安全系数太低！");
-                                }                               
-                                if (!u.UserPassword.Equals(CreateMD5.EncryptWithMD5(viewModelRetrievePsw.NewPassword)))
-                                {
-                                    if (viewModelRetrievePsw.NewPassword.Equals(viewModelRetrievePsw.SurePassword))//判断密码与确认密码是否相等
-                                    {
-
-                                        u.UserPassword = CreateMD5.EncryptWithMD5(viewModelRetrievePsw.NewPassword);
+                        if (u.UserAnswer.Equals(viewModelRetrievePsw.QuestionOrAnswer))
+                        {                            
+                                //密码怎么样，由UI去判断，减少服务端的 载荷压力，包括下面的 密码一致，2次密码是否一致，都在 UI 里决解，暂时只有账号判断在 服务端 进行                                                                           
+                                if (!u.UserPassword.Equals(viewModelRetrievePsw.NewPassword))
+                                {                                  
+                                        u.UserPassword =viewModelRetrievePsw.NewPassword;
                                         unitOfWork.SysUserRepository.Update(u);//重置密码
                                         unitOfWork.Save();
-                                        throw new Exception("密码重置成功");
-                                    }
-                                    else
-                                    {
-                                        throw new Exception("两次输入的密码不一致！");
-                                    }
+                                        throw new Exception("密码重置成功");                                
                                 }
                                 else
                                 {
                                     throw new Exception("新密码不能与原密码一致！");
-                                }
-
-
-                            }
-                            else
-                            {
-                                throw new Exception("新密码不能为空！");
-                            }
+                                }                                                      
                         }
                         else
                         {
                             throw new Exception("密码拾回问题答案错误");
-                        }
-                    }
-                    else
-                    {
-                        throw new Exception("请您输入密码拾回问题答案");
-                    }
+                        }                  
                 }
                 else
                 {
                     throw new Exception("账号不存在！");
                 }
-
-
             }
             catch (Exception ex)
             {
